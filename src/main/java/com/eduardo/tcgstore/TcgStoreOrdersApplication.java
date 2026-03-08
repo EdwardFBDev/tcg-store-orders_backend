889a80abf6fc77e57c36c39df4f34c5500c5f8ac
EdwardFBDev <edwardfb94@gmail.com>
2026-03-08 11:06:51 -0600

    Initial_commit

---
+++ src/main/java/com/eduardo/tcgstore/TcgStoreOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.tcgstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgStoreOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgStoreOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/tcgstore/TcgStoreOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.tcgstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgStoreOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
